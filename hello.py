71ad32e15c07de664055135fc1e98e538da5bdd3
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-06-13 16:51:59 +0700

    This is first python program in the book it is use for ask user name and age

---
+++ hello.py
@@ -0,0 +1,12 @@
+# This program says hello and asks for my name.  
+
+print("Hello, world!")
+print("What is your name?") # Ask for their name
+my_name = input("> ")
+print("It is a good to meet you, "+ my_name)
+print("The length of you name is: ", end=" ")
+print(len(my_name))
+print("What is your age?") # Ask for their age.
+my_age = input("> ")
+print("You will be " + str(int(my_age) + 1) + " in a year.")
+
