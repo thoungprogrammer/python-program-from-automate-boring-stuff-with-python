82077f67a6938fd7255d230e1f364136d3ef5d1b
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-06-18 21:34:41 +0700

    complete vampire.py and vampire_2.py

---
+++ vampire.py
@@ -0,0 +1,11 @@
+name = "Carol"
+age = 3000
+
+if name == "Alice":
+    print("Hi, Alice.")
+elif age < 12:
+    print("You are not Alice, kiddo.")
+elif age > 2000:
+    print("Unlike you, Alice is not an undead, immportal vamipite.")
+elif age > 100:
+    print("You are not Alice, grannie.")
